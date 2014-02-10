76b6bd1144f11c7ae9ab6439b003f9b3e47a5cd6
grant <grantpeltier93@gmail.com>
2014-02-10 17:44:29 -0600

    Updated the Driver to make it simpler and more modular

--- Driver.java
+++ Driver.java
@@ -18,58 +18,106 @@ public class Driver {
 		Customer[] custDB = new Customer[NUMCUST];
 		customerInit(custDB);											
 		while(isDone == false){
-		String input = new String(receiveTransaction());
-		if(input.indexOf("G") != -1){									// Terminate after output if G transaction code is offered
+		String output = new String(receiveTransaction(custDB));
+		JOptionPane.showMessageDialog(null, output);
+		String keepGoing = JOptionPane.showInputDialog("Would you like to do another transaction? (Y/N)");
+		if(keepGoing.indexOf("N") != -1 || keepGoing.indexOf("n") != -1){
 			isDone = true;
-		}
-		String output = executeTransaction(input);
-		if(!output.isEmpty()){											// Non-empty strings require output
-			JOptionPane.showMessageDialog(null, output);
 			}
 		}
+		displaySummaries(custDB);
 	}
 	
-	public static String receiveTransaction(){
+	/**
+	 * 
+	 * @param data
+	 * @return
+	 */
+	
+	public static String receiveTransaction(Customer[] data){
 		String output = new String();
 		String custNum = new String(JOptionPane.showInputDialog("Input Customer Number"));
 		int check = Integer.parseInt(custNum);
 		if(check < 0 || check > NUMCUST){
 			JOptionPane.showMessageDialog(null, "Invalid Customer Number","Error",JOptionPane.ERROR_MESSAGE);
-			output = receiveTransaction();
+			output = receiveTransaction(data);
 			return output;
 		}
+		Customer currentCust = data[check-1];
 		String trans = new String(JOptionPane.showInputDialog("Input Transaction Type\n(uppercase only please)"));
-		output += custNum + " " + trans;
 		if(trans.indexOf("T") != -1){
 			String amt = new String(JOptionPane.showInputDialog("Input Amount"));
 			String from = new String(JOptionPane.showInputDialog("Input Account to Transfer From (C,S,A,L)"));
 			String to = new String(JOptionPane.showInputDialog("Input Account to Transfer To (C,S,A,L)"));
-			output += " " + amt + " " + from + " " + to;
-		}else if(trans.indexOf("W") != -1 || trans.indexOf("D") != -1){
+			return transfer(currentCust, amt, from, to);
+		}else if(trans.indexOf("W") != -1){
 			String amt = new String(JOptionPane.showInputDialog("Input Amount"));
-			String acc = new String(JOptionPane.showInputDialog("Input Account (C,S,A,L)"));
-			output += " " +  amt + " " + acc;
-		}else if(trans.indexOf("I") != -1 || trans.indexOf("G") != -1){
-			String acc = new String(JOptionPane.showInputDialog("Input Account (C,S,A,L)"));
-			output += " " +  acc;
+			String acct = new String(JOptionPane.showInputDialog("Input Account (C,S,A,L)"));
+			return withdraw(currentCust, amt, acct);
+		}else if(trans.indexOf("D") != -1){
+			String amt = new String(JOptionPane.showInputDialog("Input Amount"));
+			String acct = new String(JOptionPane.showInputDialog("Input Account (C,S,A,L)"));
+			return deposit(currentCust, amt, acct);
+		}else if(trans.indexOf("I") != -1){
+			String acct = new String(JOptionPane.showInputDialog("Input Account (C,S,A,L)"));
+			return interest(currentCust, acct);
+		}else if(trans.indexOf("G") != -1){
+			String acct = new String(JOptionPane.showInputDialog("Input Account (C,S,A,L)"));
+			return summarize(currentCust, acct);
 		}else{
 			JOptionPane.showMessageDialog(null, "Invalid Transaction Type","Error",JOptionPane.ERROR_MESSAGE);
-			output = receiveTransaction();
+			output = receiveTransaction(data);
 			}
 		return output;
 	}
 	
+	/**
+	 * 
+	 */
+	public static void displaySummaries(Customer[] db){
+		
+	}
+	
+	/**
+	 * 
+	 * @param person
+	 * @param accType
+	 * @return
+	 */
 	public static BankAccount getAccount(Customer person, String accType){
 		return person.locateAccount(accType);
 	}
 	
-	public static String executeTransaction(String in){
-		String out = new String();
-		int custNum = Character.getNumericValue(in.charAt(CUSTOFF));
-		
-		return out;
+	//TODO: independent methods for every transaction
+	public static String transfer(Customer person, String amt, String from, String to){
+		String output = new String();
+		return output;
 	}
 	
+	public static String deposit(Customer person, String amt, String acct){
+		String output = new String();
+		return output;
+	}
+	
+	public static String withdraw(Customer person, String amt, String acct){
+		String output = new String();
+		return output;
+	}
+	
+	public static String interest(Customer person, String acct){
+		String output = new String();
+		return output;
+	}
+	
+	public static String summarize(Customer person, String acct){
+		String output = new String();
+		return output;
+	}
+	
+	/**
+	 * 
+	 * @param custDB
+	 */
 	public static void customerInit(Customer[] custDB){
 		BankAccount[] custAccounts1 = new BankAccount[4];
 		BankAccount[] custAccounts2 = new BankAccount[4];
